b224bbfa327dfe7de7adaab332deb560e102bf25
Aakif9866 <aakif9866@gmail.com>
2025-07-04 05:54:57 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Information Retrieval system',
+    version= '0.0.0',
+    author= 'Aakif',
+    author_email= 'aakif9866@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
